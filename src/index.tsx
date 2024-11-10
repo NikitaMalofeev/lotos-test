c07a49dc74c4d2224e7d033c60d98e1ec8b96e89
NikitaMalofeev <nikita.m.edu@gmail.com>
2024-11-10 21:56:58 +0500

    add basename to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='https://nikitamalofeev.github.io/lotos-test'>
         <App />
       </BrowserRouter>
     </Provider>
